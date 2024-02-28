a6a91269e4a1b10273df9fb39fdb0253d1db2228
Luigi Donadel <luigi.donadel@gmail.com>
2024-02-28 18:14:44 +0100

    image retrieval

--- api/index.py
+++ api/index.py
@@ -4,14 +4,15 @@ from flask import Response, stream_with_context, Flask, render_template, request
 from flask_cors import CORS
 from openai import OpenAI
 import time
+from googlesearch import search
+from bs4 import BeautifulSoup
+from multiprocessing import Pool
+
+import requests
 
 client = OpenAI(organization="org-CfSBGlGQ3mG4LSEYPfp1Yy4o")
 app = Flask(__name__)
 CORS(app)
-
-
-def get_image(context):
-    return "<img src='https://img.freepik.com/free-psd/google-icon-isolated-3d-render-illustration_47987-9777.jpg?size=338&ext=jpg&ga=GA1.1.1141335507.1707609600&semt=ais'/>"
 
 def enhance_result(output, pattern):
     print(output)
@@ -25,6 +26,7 @@ def enhance_result(output, pattern):
         output = output.replace(i, o)
     return output
 
+
 def explanationPrompt(argument):
     return f""""
     Explain the following concept: {argument} in a concise and simple way, like I'm 5 years old. 
@@ -35,13 +37,17 @@ def imagePrompt(p):
     Given this paragraph: "{p}"
     Generate a query to search on Google Images for an image that can clarify content.
     """
-    
+def boldPrompt(p):
+    return f""""
+    Given this paragraph: "{p}"
+    Return the same with bold tags on some key words.
+    """
 def ask(prompt):
     return client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content": prompt}
-        ])
+        ]).choices[0].message.content
 
 def askArgument(prompt):
     return client.chat.completions.create(
@@ -52,25 +58,59 @@ def askArgument(prompt):
             {"role": "user", "content": prompt}
         ])
 
-def format(paragraph):
-    pass
+def get_image(prompt):
+    try:
+        # Perform Google search
+        url = rf'https://www.google.no/search?q={prompt}&client=opera&hs=cTQ&source=lnms&tbm=isch&sa=X&safe=active&ved=0ahUKEwig3LOx4PzKAhWGFywKHZyZAAgQ_AUIBygB&biw=1920&bih=982'
+
+        page = requests.get(url).text
+
+        soup = BeautifulSoup(page, 'html.parser')
+
+        for raw_img in soup.find_all('img'):
+            link = raw_img.get('src')
+    
+            if link and link.startswith("https://"):
+                return link
+        
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None
+
+def transform(paragraph):
+    paragraph = ask(boldPrompt(paragraph))
+    image_keys = ask(imagePrompt(paragraph))
+    image = get_image(image_keys)
+    if image:
+        paragraph = f"{paragraph}<br/><img src=\"{image}\"/>"
+
+    paragraph = f"{paragraph}<br/>"
+    return paragraph
 
 @app.route("/api/search", methods=["POST"])
-def search():
+def searchArg():
     argument = request.json["query"]
 
     prompt = explanationPrompt(argument)
 
     def askPrompt():
-        collected_messages = []
-        for chunk in ask(prompt, stream=True):
+        full_reply_content = ""
+        for chunk in askArgument(prompt):
             chunk_message = chunk.choices[0].delta.content  # extract the message
             #collected_messages.append(chunk_message)
             #collected_messages = [m for m in collected_messages if m is not None]
             #full_reply_content = ''.join([m for m in collected_messages])
             if chunk_message:
-                print(chunk_message, flush=True, end="")
-                yield f"{chunk_message}"
+                #print(chunk_message, flush=True, end="")
+                full_reply_content = f"{full_reply_content}{chunk_message}"
+        
+        soup = BeautifulSoup(full_reply_content, 'html.parser')
+        # Trova tutti i tag <p> nella pagina e ottieni solo il loro HTML
+        with Pool() as pool:
+            transformed_paragraphs = pool.map(transform, [str(p) for p in soup.find_all('p')])
+            
+        paragraphs_html = "".join(transformed_paragraphs)
+        yield paragraphs_html
     
     return Response(askPrompt(), mimetype='text/event-stream')
 
