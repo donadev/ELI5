0079b99b9943edda6e26d16f4224f90ac457916b
Luigi Donadel <luigi.donadel@gmail.com>
2024-03-02 19:23:34 +0100

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { kv } from "@vercel/kv";
 import FormBubble from "@/components/FormBubble";
 import { NextRequest } from 'next/server'
 
-const Home: React.FC = async () => {
+const Home: React.FC = () => {
   const results = useStore((state) => state.results);
   const getIp = useStore((state) => state.get_ip);
   const [visits, setVisits] = useState(0)
@@ -29,7 +29,6 @@ const Home: React.FC = async () => {
       {
         (visits > 5) ? <FormBubble/> : null
       }
-      <FormBubble/>
       <h1 className="text-2xl font-bold mb-4">Explain it Like I'm 5</h1>
       <h2 className="text-1xl font-regular mb-3">Choose an argument and learn it with very simple principles</h2>
 
