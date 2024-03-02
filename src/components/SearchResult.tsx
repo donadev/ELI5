331fdddc6a2690502a8c847f452ddf7ffab5a28c
Luigi Donadel <luigi.donadel@gmail.com>
2024-03-02 21:38:16 +0100

    share

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,22 +1,33 @@
-import { useStore } from "@/store";
+import { baseUrl, useStore } from "@/store";
 import { useState } from "react";
 
 interface SearchResultProps {
   result: {
     query: string,
+    status: string,
     message: string;
   };
 }
 
 const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
   const [loading, setLoading] = useState(false);
+  const [copied, setCopied] = useState(false)
   
+  function share() {
+
+    navigator.clipboard.writeText(baseUrl + "?q=" + encodeURIComponent(result.query));
+    setCopied(true)
+  }
 
   return (
     <div className="flex flex-col items-center space-x-2 mb-2">
         <h1 className="text-xl font-bold mb-4">{result.query}</h1>
         <p dangerouslySetInnerHTML={{ __html: result.message }}></p>
-        <br/><br/>
+        <br/>
+        { result.status == "ended" ? 
+        (<button className="px-2 py-1 text-white rounded bg-sky-500" onClick={share}>{copied ? "Copied" : "Share link"}</button>) : null
+        }
+        <br/>
     </div>
   );
 };
--- src/store.ts
+++ src/store.ts
@@ -24,7 +24,7 @@ type QueryStore = {
   openForm: () => void
 };
 
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+export const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
 ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 : "http://localhost:3000";
 
